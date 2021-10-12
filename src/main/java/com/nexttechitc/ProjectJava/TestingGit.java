759240a186439fad15cff87846f39c4dd674e319
selina <abdg@gmail.com>
2021-10-12 00:38:29 -0500

    code added

--- src/main/java/com/nexttechitc/ProjectJava/TestingGit.java
+++ src/main/java/com/nexttechitc/ProjectJava/TestingGit.java
@@ -4,7 +4,7 @@ public class TestingGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i'm from Texas");
+		System.out.println("i'm from Irving");
 		
 
 	}
